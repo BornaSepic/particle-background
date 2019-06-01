24f410b510258e8f23cff4c639ea9043015bd027
Borna S <bornasepic98@gmail.com>
2019-06-01 14:27:09 +0200

    :rocket: Finalizing particle effect

--- particle-effect.js
+++ particle-effect.js
@@ -1,15 +1,26 @@
 const canvas = document.querySelector('#particle-effect-container');
+const c = canvas.getContext('2d');
 
 const maxWidth = window.innerWidth;
 const maxHeight = window.innerHeight;
 
 canvas.height = maxHeight;
 canvas.width = maxWidth;
-const c = canvas.getContext('2d');
+c.fillStyle = "rgba(245, 245, 245, 0.8)";
 
-c.fillStyle = "#00000";
+const xVel = 1;
+const yVel = 1;
+const distanceTreshold = 90;
 
-const circleRad = 5;
+const circleRad = 3;
+
+class Line {
+    constructor(startPoint, endPoint, opacity) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+        this.opacity = opacity;
+    }
+}
 
 class Point {
     constructor(xCord, yCord, xVel, yVel) {
@@ -19,8 +30,8 @@ class Point {
         this.xCurCord = xCord;
         this.yCurCord = yCord;
 
-        this.xVel = xVel;
-        this.yVel = yVel;
+        this.xVel = xVel * Math.random();
+        this.yVel = yVel * Math.random();
     }
 
     move() {
@@ -29,7 +40,7 @@ class Point {
         }
 
         if(this.yCurCord < 0 || this.yCurCord > maxHeight) {
-            this.yVel = this.yVel * -1;
+            this.yVel = this.yVel * -1.1;
         }
 
         this.xCurCord = this.xCurCord + this.xVel;
@@ -39,14 +50,20 @@ class Point {
 
 const points = [];
 
-for (let i = 0; i < 20; i++) {
-    points.push(new Point(Math.random() * maxWidth, Math.random() * maxHeight, Math.random() < 0.5 ? 2 : -2, Math.random() < 0.5 ? 1.5 : -1.5))
-}
+for (let i = 0; i < 35; i++) {
+    points.push(new Point(
+        Math.random() * maxWidth, 
+        Math.random() * maxHeight, 
+        Math.random() < 0.5 ? xVel : -xVel, 
+        Math.random() < 0.5 ? yVel : -yVel)
+    );
+};
 
 function animateCircles() {
+    const lines = [];
     c.clearRect(0, 0, maxWidth, maxHeight);
 
-    points.map((point) => {
+    points.map(point => {
         c.beginPath();
         c.arc(point.xCurCord, point.yCurCord, circleRad, 0, 2 * Math.PI, false);
         c.fill();
@@ -55,11 +72,42 @@ function animateCircles() {
         point.move();
     });
 
+    points.map(mainPoint => {
+        let mainX = mainPoint.xCurCord;
+        let mainY = mainPoint.yCurCord;
+
+        points.map(point => {
+            let xDif = Math.abs(point.xCurCord - mainX);
+            if (xDif === 0 || xDif > distanceTreshold) {
+                return
+            };
+
+            let yDif = Math.abs(point.yCurCord - mainY);
+            if (yDif === 0 || yDif > distanceTreshold) {
+                return 
+            };
+
+            if(xDif < distanceTreshold &&  yDif < distanceTreshold) {
+                lines.push({
+                    startPoint: {x: mainX, y: mainY}, 
+                    endPoint: {x: point.xCurCord, y: point.yCurCord}, 
+                    opacity: 1 - ((xDif + yDif) / 100)
+                });
+            };
+        });
+    });
+
+    lines.map(line => {
+        c.beginPath();
+        c.moveTo(line.startPoint.x, line.startPoint.y);
+        c.lineTo(line.endPoint.x, line.endPoint.y);
+        c.strokeStyle = `rgba(245, 245, 245, ${line.opacity})`;
+        c.stroke();
+    });
+
     requestAnimationFrame(function() {
-        //animateCircles();
+        animateCircles();
     });
 }
 
 animateCircles();
-
-
