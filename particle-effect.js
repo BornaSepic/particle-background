b78b574d87ef8761f086cdada9dca956286a2611
Borna S <bornasepic98@gmail.com>
2019-06-02 22:54:33 +0200

    :lipstick: Improved responsiveness, added colors to the particles

--- particle-effect.js
+++ particle-effect.js
@@ -1,18 +1,26 @@
 const canvas = document.querySelector('#particle-effect-container');
 const c = canvas.getContext('2d');
 
-const maxWidth = window.innerWidth;
-const maxHeight = window.innerHeight;
+let maxWidth = window.innerWidth;
+let maxHeight = window.innerHeight;
 
 canvas.height = maxHeight;
 canvas.width = maxWidth;
-c.fillStyle = "rgba(245, 245, 245, 0.8)";
+
+const particlesCount = maxWidth / 20;
+
+const colorCombos =[
+    '#ff7553', '#40fff2',
+    '#a14aff', '#f3ff4a',
+    '#646cff', '#ff8120',
+    '#ff00d0', '#3cff00'
+];
 
 const xVel = 1;
 const yVel = 1;
 const distanceTreshold = 90;
 
-const circleRad = 3;
+const circleRad = 2;
 
 class Line {
     constructor(startPoint, endPoint, opacity) {
@@ -32,15 +40,17 @@ class Point {
 
         this.xVel = xVel * Math.random();
         this.yVel = yVel * Math.random();
+
+        this.color = colorCombos[Math.round(Math.random() * 7)];
     }
 
     move() {
-        if(this.xCurCord < 0 || this.xCurCord > maxWidth) {
+        if(this.xCurCord <= 0 || this.xCurCord > maxWidth) {
             this.xVel = this.xVel * -1;
         }
 
-        if(this.yCurCord < 0 || this.yCurCord > maxHeight) {
-            this.yVel = this.yVel * -1.1;
+        if(this.yCurCord <= 0 || this.yCurCord > maxHeight) {
+            this.yVel = this.yVel * -1;
         }
 
         this.xCurCord = this.xCurCord + this.xVel;
@@ -50,7 +60,7 @@ class Point {
 
 const points = [];
 
-for (let i = 0; i < 35; i++) {
+for (let i = 0; i < particlesCount; i++) {
     points.push(new Point(
         Math.random() * maxWidth, 
         Math.random() * maxHeight, 
@@ -66,6 +76,7 @@ function animateCircles() {
     points.map(point => {
         c.beginPath();
         c.arc(point.xCurCord, point.yCurCord, circleRad, 0, 2 * Math.PI, false);
+        c.fillStyle = point.color;
         c.fill();
         c.stroke();
 
@@ -91,7 +102,8 @@ function animateCircles() {
                 lines.push({
                     startPoint: {x: mainX, y: mainY}, 
                     endPoint: {x: point.xCurCord, y: point.yCurCord}, 
-                    opacity: 1 - ((xDif + yDif) / 100)
+                    opacity: 1 - ((xDif + yDif) / 100),
+                    pointColors: [mainPoint.color, point.color]
                 });
             };
         });
@@ -101,7 +113,12 @@ function animateCircles() {
         c.beginPath();
         c.moveTo(line.startPoint.x, line.startPoint.y);
         c.lineTo(line.endPoint.x, line.endPoint.y);
-        c.strokeStyle = `rgba(245, 245, 245, ${line.opacity})`;
+
+        const gradient = c.createLinearGradient(line.startPoint.x, line.startPoint.y, line.endPoint.x, line.endPoint.y);
+        gradient.addColorStop("0", line.pointColors[0]);
+        gradient.addColorStop("1", line.pointColors[1]);
+
+        c.strokeStyle = gradient;
         c.stroke();
     });
 
@@ -111,3 +128,12 @@ function animateCircles() {
 }
 
 animateCircles();
+
+
+window.addEventListener("resize", event => {
+    canvas.height = event.target.window.innerHeight;
+    canvas.width = event.target.window.innerWidth;
+
+    maxHeight = event.target.window.innerHeight;
+    maxWidth = event.target.window.innerWidth;
+});
